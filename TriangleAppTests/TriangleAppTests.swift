85df08c70d45b017a68af4a94a13f6510d05150c
samberutu <45347400+samberutu@users.noreply.github.com>
2021-11-28 12:06:01 +0700

    add segitiga siku

--- TriangleAppTests/TriangleAppTests.swift
+++ TriangleAppTests/TriangleAppTests.swift
@@ -17,14 +17,15 @@ class TriangleAppTests: XCTestCase {
     }
     func testDetectEquilateralTriangle() {
         XCTAssertEqual(try detectTriangle(1, 2, 2), "Segitiga Sama Kaki")
-        XCTAssertEqual(try detectTriangle(1, 1, 2), "Segitiga Sama Kaki")
-        XCTAssertEqual(try detectTriangle(1, 2, 1), "Segitiga Sama Kaki")
         XCTAssertEqual(try detectTriangle(1, 2, 2), "Segitiga Sama Kaki")
         XCTAssertEqual(try detectTriangle(2, 1, 2), "Segitiga Sama Kaki")
     }
     
     func testDetectRandomTriangle() {
-        XCTAssertEqual(try detectTriangle(1, 6, 4), "Segitiga Sembarang")
+        XCTAssertEqual(try detectTriangle(3, 5, 7), "Segitiga Sembarang")
+    }
+    func testDetectPythagorasTriangle() {
+      XCTAssertEqual(try detectTriangle(6, 8, 10), "Segitiga Siku-Siku")
     }
     
     func detectTriangle(
@@ -40,10 +41,14 @@ class TriangleAppTests: XCTestCase {
           }
         }
        
-        if sides[0] == sides[1] && sides[0] == sides[2] {
+        if sides[1] + sides[0] <= sides[2] {
+          throw TriangleError.inequalityInput
+        } else if sides[0] == sides[1] && sides[0] == sides[2] {
           return "Segitiga Sama Sisi"
         } else if sides[0] == sides[1] || sides[1] == sides[2] {
           return "Segitiga Sama Kaki"
+        } else if Double((sides[0] * sides[0] + sides[1] * sides[1])).squareRoot() == Double(sides[2]) {
+          return "Segitiga Siku-Siku"
         } else {
           return "Segitiga Sembarang"
         }
